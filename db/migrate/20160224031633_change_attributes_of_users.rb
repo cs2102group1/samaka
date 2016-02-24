85f023e2e89c8481375c1407319feeccee736010
Mai Anh Vu <me@maianhvu.com>
2016-02-24 12:13:28 +0800

    change faulty database migrations

--- db/migrate/20160224031633_change_attributes_of_users.rb
+++ db/migrate/20160224031633_change_attributes_of_users.rb
@@ -1,6 +1,5 @@
 class ChangeAttributesOfUsers < ActiveRecord::Migration
   def change
     change_column :users, :role, :string, null: false, default: User::STRING_ROLE_MEMBER
-    remove_column :users, :admin
   end
 end
