e209cc83e568df795351aaf509e22427aa871320
Jason Chau <cvansang94@gmail.com>
2016-03-19 12:08:09 +0800

    Revert "Delete query for user model"
    This reverts commit 2c5e248d25ea424c30966f613a375c47f1a55d2b.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,18 +1,4 @@
 class Driver < ActiveRecord::Base
   belongs_to :user
   has_many :journeys, dependent: :destroy
-
-  def self.all
-    query = "SELECT * FROM drivers"
-    self.find_by_sql(query)
-  end
-
-  def self.find(params)
-    query = <<-FIND
-            SELECT * FROM journeys j WHERE
-            j.pickup_point = '#{params[:car_plate]}' AND
-            j.start_time = '#{params[:start_time]}';
-            FIND
-    self.find_by_sql(query);
-  end
 end
