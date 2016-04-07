6172d7dc28e5506c275829925e93dd0f05141290
Karen Ang <thekarenang@gmail.com>
2016-04-07 23:40:06 +0800

    Update passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,10 +1,9 @@
 class Passenger < ActiveRecord::Base
-  belongs_to :user
   has_many :journeys, dependent: :destroy
 
-  def self.all
-    query = "SELECT * FROM passengers"
-    self.find_by_sql(query)
+  def self.insert(params)
+    query = "INSERT INTO passengers (email, start_time, car_plate, onboard) VALUES('#{params[:email]}', '#{params[:start_time]}', '#{params[:car_plate]}', '#{params[:onboard]}');"
+    ActiveRecord::Base.connection.execute(query)
   end
 
   def self.find(params)
@@ -16,7 +15,7 @@ class Passenger < ActiveRecord::Base
     self.find_by_sql(query)
   end
 
-  def self.update(params)
+  def self.update(params, email)
     values = []
     columns = params.keys.each do |k|
       values << "#{k.to_s} = '#{params[k]}'" unless k == 'start_time' ||
@@ -26,8 +25,15 @@ class Passenger < ActiveRecord::Base
     query = <<-UPDATE_P
             UPDATE passengers
             SET #{update_values}
-            WHERE start_time = #{params[:start_time]}
-            AND car_plate = #{params[:car_plate]}
+            WHERE start_time = '#{params[:start_time]}'
+            AND car_plate = '#{params[:car_plate]}'
+            AND email = '#{email}'
             UPDATE_P
+    ActiveRecord::Base.connection.execute(query)
+  end
+
+  def self.check(passenger, j)
+    query = "SELECT * FROM passengers WHERE start_time = '#{j.start_time}' AND car_plate = '#{j.car_plate}' AND email = '#{passenger}' AND onboard = TRUE;"
+    !ActiveRecord::Base.connection.execute(query).values.empty?
   end
 end
