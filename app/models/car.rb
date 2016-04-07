7f601391a63f786f772abb653a176a4d02274d39
Karen Ang <thekarenang@gmail.com>
2016-04-07 22:39:16 +0800

    Update Car model for edit an delete queries

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,13 +1,24 @@
 class Car < ActiveRecord::Base
-  belongs_to :user
   def self.insert(params)
     query = "INSERT INTO cars (car_plate, owner)
             VALUES('#{params[:car_plate]}', '#{params[:owner]}');"
     ActiveRecord::Base.connection.execute(query)
   end
 
-  def self.find(user_email)
-    query = "SELECT car_plate FROM cars c WHERE c.owner = '#{user_email}';"
+  def self.find(options={})
+    query = "SELECT car_plate FROM cars c WHERE c.owner = '#{options[:owner]}' OR c.car_plate = '#{options[:car_plate]}';"
     self.find_by_sql(query)
   end
+
+  def self.update(p={})
+    Car.delete(car_plate: p[:old])
+    ins = "INSERT INTO cars (car_plate, owner) VALUES('#{p[:new]}', '#{p[:owner]}');"
+    ActiveRecord::Base.connection.execute(ins)
+  end
+
+  def self.delete(p={})
+    del = "DELETE FROM cars WHERE car_plate = '#{p[:car_plate]}';"
+    ActiveRecord::Base.connection.execute(del)
+  end
+
 end
