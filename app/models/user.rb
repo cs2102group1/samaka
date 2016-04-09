0ea0f53dddc4b43fce68fff1827f2a6c4da4fc77
Karen Ang <thekarenang@gmail.com>
2016-04-10 00:47:19 +0800

    Add more advanced SQL for leaderboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,15 +20,42 @@ class User < ActiveRecord::Base
     ActiveRecord::Base.connection.execute(query).values.flatten
   end
 
-  def self.leaderboard
-    query = <<-LEADERBOARD
+  def self.top_spenders
+    query = <<-SPENDER
             SELECT u.username, MAX(j.price)
             FROM users u, passengers p, journeys j
             WHERE u.email = p.email
             AND j.start_time = p.start_time
+            AND j.car_plate = p.car_plate
             GROUP BY u.email, u.username
             ORDER BY MAX(j.price) DESC;
-            LEADERBOARD
+            SPENDER
+    ActiveRecord::Base.connection.execute(query).values
+  end
+
+  def self.frequent_riders
+    query = <<-FREQ
+            SELECT u.username, COUNT(*)
+            FROM users u, passengers p, journeys j
+            WHERE u.email = p.email
+            AND j.start_time = p.start_time
+            AND j.car_plate = p.car_plate
+            GROUP BY u.email, u.username
+            ORDER BY COUNT(*) DESC;
+            FREQ
+    ActiveRecord::Base.connection.execute(query).values
+  end
+
+  def self.super_drivers
+    query = <<-SUDRV
+            SELECT u.username, COUNT(*)
+            FROM users u, journeys j, drivers d
+            WHERE u.email = d.email
+            AND j.start_time = d.start_time
+            AND j.car_plate = d.car_plate
+            GROUP BY u.email, u.username
+            ORDER BY COUNT(*) DESC;
+            SUDRV
     ActiveRecord::Base.connection.execute(query).values
   end
 
