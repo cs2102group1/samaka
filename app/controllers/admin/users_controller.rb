8c594364fc729448f27f7dd8b5706e06f90d6a19
Karen Ang <thekarenang@gmail.com>
2016-04-10 00:47:36 +0800

    Display leaderboard based on different query params

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,19 @@
 class Admin::UsersController < ApplicationController
   layout 'subpage'
   def index
-    @users = User.leaderboard
+    @users = []
+    if params[:q]
+      case params[:q]
+      when 'top_spenders'
+        @title = 'Maximum spent on a ride'
+        @users = User.top_spenders
+      when 'frequent_riders'
+        @title = 'Number of rides taken'
+        @users = User.frequent_riders
+      when 'super_drivers'
+        @title = 'Number of rides offered'
+        @users = User.super_drivers
+      end
+    end
   end
 end
