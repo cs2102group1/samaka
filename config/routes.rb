45a2bd132db932c49dc1609548cfb98883e558bb
Jason Chau <cvansang94@gmail.com>
2016-04-11 01:27:24 +0800

    Route for request

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :users
+    resources :requests
   end
 
   get 'journeys', to: 'journeys#index'
@@ -25,5 +26,6 @@ Rails.application.routes.draw do
   end
 
   resources :cars, param: "car_plate", except: [:show]
+  resources :requests, only: [:new, :create]
 
 end
